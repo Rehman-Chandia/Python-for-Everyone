f90205e1037acd6512c94f5fe57b124962822e35
Rehman Chandia <itsmerehman.official@gmail.com>
2026-09-02 15:43:39 +0000

    Add input-output example in Python

---
+++ input-output-variables.py
@@ -0,0 +1,8 @@
+# My First Python Program
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("You are", age, "years old.")
+print("Welcome to Python!")
