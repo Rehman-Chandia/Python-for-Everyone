4dfd47944232967094c8c55a4976b6358dbb944b
Rehman Chandia <itsmerehman.official@gmail.com>
2026-09-04 14:56:42 +0500

    Add port-based conditional statements

---
+++ if and elif.py
@@ -0,0 +1,9 @@
+port = int(input("enter port: "))
+if port == 34:
+    print("rehman")
+elif port == 478:
+    print("fuck you")
+elif port == 54:
+    print("CHANDIA")
+else:
+    print("wahab wants to be a scientists")
