b0ce32f5c405b2cd28c0ca935cdddcc94d9717f1
Rehman Chandia <itsmerehman.official@gmail.com>
2026-09-06 18:34:54 +0000

    Add port number input and loop for validation

---
+++ Finding port num using loop.py
@@ -0,0 +1,8 @@
+#use of port and loop
+port = int(input("enter port number: "))
+for p in range(20, 60):
+    if p == port:
+        print("port is:", p)
+    else:
+        print("not fou")
+        break
